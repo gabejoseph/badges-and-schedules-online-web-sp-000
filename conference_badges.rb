0ae7026d0ed889952c21bf079c4567445ebb8680
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-03 18:47:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  out = []
+  attendees.each do |x|
+    out << "Hello, my name is #{x}."
+  end
+  return out
+end
+
+
+def assign_rooms(attendees)
+  outie = []
+  attendees.each_with_index do |name, index|
+    outie << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+  return outie
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees)
+end
+
